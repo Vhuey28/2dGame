a64020b06923d79b37ceaf5c24d7f5feaf6c35f9
Vaughn Huey <vhuey29@ggmail.com>
2026-07-13 16:35:53 -0700

    Name change

--- my2Dgame/src/my2Dgame/Main.java
+++ my2Dgame/src/my2Dgame/Main.java
@@ -9,7 +9,7 @@ public class Main {
 		JFrame window  = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
-		window.setTitle("Retrubtion Beta");
+		window.setTitle("Chronicle Conquest Beta");
 		
 		GamePanel gamePanel = new GamePanel();
 		window.add(gamePanel);
